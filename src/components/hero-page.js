7ef564462cc397828cdc2f2362d1cf0890b2abf8
Dallas Palumbo <palumbodallas@gmail.com>
2022-12-05 17:42:33 -0500

    working loading

--- src/components/hero-page.js
+++ src/components/hero-page.js
@@ -1,37 +1,62 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { RingLoader } from "react-spinners";
 import BarChart from "./chart";
+import "../css/hero-page.css"
 
 
 function HeroPage(){
 
 const[hero, setHero] = useState({})
+const[loading, setLoading] = useState(true)
     
 let{id} = useParams()
 
+// triggers loader while fetching info then turns it off after setting state for hero.
     useEffect(()=>{
         async function fetch_heros(id){
-           const response = await fetch(`http://localhost:4000/${id}`) 
-           const resData = await response.json();
-           setHero(resData)
-           }
+            const response = await fetch(`http://localhost:4000/${id}`) 
+            const resData = await response.json();
+            setHero(resData)
+            setLoading(false)
+            }
            fetch_heros(id)
    },[])
 
-// console.log(hero.biography["place-of-birth"])
+
+console.log(hero)
 
     return(
         <div>
-            <h1>HeroPage</h1>
-            <h2>{hero.name}</h2>
-            {/* <p>{hero.biography.place-of-birth}</p> */}
+            {
+                loading ? 
+                <RingLoader
+                color="#0000FF"
+                loading={loading}
+                size={150}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+                />
+                :
+            <div>
+            <h1>{hero.name}</h1>
+            {/* <p>{hero.biography["place-of-birth"]}</p> */}
             <div className="hero">
-            <img src={hero.image["url"]} alt={hero.name}/>
-
+            <img id="hero-img" src={hero.image["url"]} alt={hero.name}/>
+            <div className="identity">
+            <h2><u>{hero.biography.alignment == "good" ? "Hero" : "Villian"}</u></h2>
+            <ul>
+                <li>Full Name : {hero.biography["full-name"]}</li>
+                <li>Eye color : {hero.appearance["eye-color"]}</li>
+                <li>Height: {hero.appearance.height.length > 1 ? hero.appearance.height.join(" Ft, ") : hero.appearance.height}</li>
+                {/* hero.appearance.height.join(" Ft, ") */}
+            </ul>
+            </div>
+            </div>
             </div>
            
-          
+        }  
         </div>
     )
 }
