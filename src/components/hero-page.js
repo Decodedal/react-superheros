2ac5c23e42d5a7a305a65f6524d2681da8e27624
Dallas Palumbo <palumbodallas@gmail.com>
2022-12-06 13:20:45 -0500

    working on hero page

--- src/components/hero-page.js
+++ src/components/hero-page.js
@@ -13,6 +13,7 @@ const[loading, setLoading] = useState(true)
     
 let{id} = useParams()
 
+
 // triggers loader while fetching info then turns it off after setting state for hero.
     useEffect(()=>{
         async function fetch_heros(id){
@@ -21,7 +22,9 @@ let{id} = useParams()
             setHero(resData)
             setLoading(false)
             }
-           fetch_heros(id)
+           
+            fetch_heros(id)
+           
    },[])
 
 
@@ -31,30 +34,43 @@ console.log(hero)
         <div>
             {
                 loading ? 
+                <div style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
                 <RingLoader
                 color="#0000FF"
                 loading={loading}
                 size={150}
+                margin = "0 auto"
                 aria-label="Loading Spinner"
                 data-testid="loader"
                 />
-                :
-            <div>
+                </div>
+                :  
+            <main>
             <h1>{hero.name}</h1>
             {/* <p>{hero.biography["place-of-birth"]}</p> */}
             <div className="hero">
             <img id="hero-img" src={hero.image["url"]} alt={hero.name}/>
             <div className="identity">
+            <div className="list">
             <h2><u>{hero.biography.alignment == "good" ? "Hero" : "Villian"}</u></h2>
             <ul>
-                <li>Full Name : {hero.biography["full-name"]}</li>
+                <li>Secret identity : {hero.biography["full-name"] == "" ? "none": hero.biography["full-name"] }</li>
+                <li>Aliases: {hero.biography.aliases.join(", ")}</li>
                 <li>Eye color : {hero.appearance["eye-color"]}</li>
-                <li>Height: {hero.appearance.height.length > 1 ? hero.appearance.height.join(" Ft, ") : hero.appearance.height}</li>
-                {/* hero.appearance.height.join(" Ft, ") */}
+                <li>Height: {hero.appearance.height[0] == "-" ? hero.appearance.height[1] : hero.appearance.height.join(" Ft, ")  }</li>
+                <li>Weight: {hero.appearance.weight.join(', ')}</li>
+                <li>Race: {hero.appearance.race}</li>
+                <li>Gender: {hero.appearance.gender}</li>
             </ul>
             </div>
+            
+            <div className="list">
+                <p>list here</p>
+                
             </div>
             </div>
+            </div>
+            </main>
            
         }  
         </div>
