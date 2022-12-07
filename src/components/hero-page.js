cc6b19578b5f8ee54c7ac26b5aa92647b4491bc8
Dallas Palumbo <palumbodallas@gmail.com>
2022-12-07 00:37:18 -0500

    chart working in place comic font added

--- src/components/hero-page.js
+++ src/components/hero-page.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { RingLoader } from "react-spinners";
-import BarChart from "./chart";
+import {BarChart} from "./barChart"
 import "../css/hero-page.css"
 
 
@@ -11,6 +11,7 @@ function HeroPage(){
 const[hero, setHero] = useState({})
 const[loading, setLoading] = useState(true)
     
+const {powerstats} = hero
 let{id} = useParams()
 
 
@@ -28,7 +29,10 @@ let{id} = useParams()
    },[])
 
 
-console.log(hero)
+// console.log(hero)
+// console.log(powerstats.strength)
+
+
 
     return(
         <div>
@@ -54,13 +58,14 @@ console.log(hero)
             <div className="list">
             <h2><u>{hero.biography.alignment == "good" ? "Hero" : "Villian"}</u></h2>
             <ul>
-                <li>Secret identity : {hero.biography["full-name"] == "" ? "none": hero.biography["full-name"] }</li>
-                <li>Aliases: {hero.biography.aliases.join(", ")}</li>
-                <li>Eye color : {hero.appearance["eye-color"]}</li>
-                <li>Height: {hero.appearance.height[0] == "-" ? hero.appearance.height[1] : hero.appearance.height.join(" Ft, ")  }</li>
-                <li>Weight: {hero.appearance.weight.join(', ')}</li>
-                <li>Race: {hero.appearance.race}</li>
-                <li>Gender: {hero.appearance.gender}</li>
+                <li>secret identity : {hero.biography["full-name"] == "" ? "none": hero.biography["full-name"].toLowerCase() }</li>
+                <li>aliases: {hero.biography.aliases.join(", ").toLowerCase()}</li>
+                <li>eye color : {hero.appearance["eye-color"]}</li>
+                <li>height: {hero.appearance.height[0] == "-" ? hero.appearance.height[1] : hero.appearance.height.join(" ft, ")  }</li>
+                <li>weight: {hero.appearance.weight.join(' , ')}</li>
+                <li>race: {hero.appearance.race.toLowerCase()}</li>
+                <li>gender: {hero.appearance.gender}</li>
+                <li>universe: {hero.biography.publisher}</li> 
             </ul>
             </div>
             
@@ -68,8 +73,25 @@ console.log(hero)
                 <p>list here</p>
                 
             </div>
+            <div className="chart">
+            <BarChart data ={
+                {
+                    labels: ['Intelligence', 'Strength', 'Speed', 'Durability', 'Power', 'Combat'],
+                    datasets: [
+                      {
+                        label: 'Power Stats',
+                        data: [powerstats.intelligence, powerstats.strength, powerstats.speed, powerstats.durability , powerstats.power, powerstats.combat],
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1,
+                      },
+                    ],
+                }
+            }/>
             </div>
             </div>
+            </div>
+      
             </main>
            
         }  
