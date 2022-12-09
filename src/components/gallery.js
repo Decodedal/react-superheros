89830bc4907d8d2a8a724db65808d28fe15a7ae0
Dallas Palumbo <palumbodallas@gmail.com>
2022-12-09 16:46:49 -0500

    working on gallery

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+const [hero, setHero] = useEffect([
+    {
+        "response": "success",
+        "id": "307",
+        "name": "Han Solo",
+        "url": "https://www.superherodb.com/pictures2/portraits/10/100/10456.jpg"
+    },
+    {
+        "response": "success",
+        "id": "285",
+        "name": "Gladiator",
+        "url": "https://www.superherodb.com/pictures2/portraits/10/100/1521.jpg"
+    },
+    {
+        "response": "success",
+        "id": "137",
+        "name": "Brainiac 5",
+        "url": "https://www.superherodb.com/pictures2/portraits/10/100/1183.jpg"
+    },
+    {
+        "response": "success",
+        "id": "99",
+        "name": "Black Cat",
+        "url": "https://www.superherodb.com/pictures2/portraits/10/100/32.jpg"
+    },
+    {
+        "response": "success",
+        "id": "89",
+        "name": "Bird-Man",
+        "url": "https://www.superherodb.com/pictures2/portraits/10/100/1503.jpg"
+    }
+])
 
 
 function Gallery(){
+
+    useEffect(()=>{
+        async function fetch_heros(){
+           const response = await fetch("http://localhost:4000/home") 
+           const resData = await response.json();
+           console.log(resData)
+           }
+           fetch_heros()
+   },[])
 
 
     return(
--- src/components/home.js
+++ src/components/home.js
@@ -3,14 +3,6 @@ import { useEffect } from "react";
 
 function Home(){
     
-    useEffect(()=>{
-         async function fetch_heros(){
-            const response = await fetch("http://localhost:4000/2") 
-            const resData = await response.json();
-            console.log(resData)
-            }
-            fetch_heros()
-    },[])
 
 return(
     <div>
