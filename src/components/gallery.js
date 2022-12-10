c3dd1d8d942e8511b1a4751c5b16959cf1a84cc5
Dallas Palumbo <palumbodallas@gmail.com>
2022-12-10 00:52:40 -0500

    loading heros from api on home page link to hero page in place

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,67 +1,73 @@
 import React from "react";
-import { useState} from "react";
+import { useState, useEffect} from "react";
 import GalleryItem from "./galleryItem";
 import '../css/gallery.css'
+import { RingLoader } from "react-spinners";
 
 
 
 function Gallery(){
 
-    
+const [loading, setLoading] = useState(false)
 const [hero, setHero] = useState([
-    {
-        "response": "success",
-        "id": "307",
-        "name": "Han Solo",
-        "url": "https://www.superherodb.com/pictures2/portraits/10/100/10456.jpg"
-    },
-    {
-        "response": "success",
-        "id": "285",
-        "name": "Gladiator",
-        "url": "https://www.superherodb.com/pictures2/portraits/10/100/1521.jpg"
-    },
-    {
-        "response": "success",
-        "id": "137",
-        "name": "Brainiac 5",
-        "url": "https://www.superherodb.com/pictures2/portraits/10/100/1183.jpg"
-    },
-    {
-        "response": "success",
-        "id": "99",
-        "name": "Black Cat",
-        "url": "https://www.superherodb.com/pictures2/portraits/10/100/32.jpg"
-    },
-    {
-        "response": "success",
-        "id": "89",
-        "name": "Bird-Man",
-        "url": "https://www.superherodb.com/pictures2/portraits/10/100/1503.jpg"
-    }
+    // {
+    //     "response": "success",
+    //     "id": "307",
+    //     "name": "Han Solo",
+    //     "url": "https://www.superherodb.com/pictures2/portraits/10/100/10456.jpg"
+    // },
+    // {
+    //     "response": "success",
+    //     "id": "285",
+    //     "name": "Gladiator",
+    //     "url": "https://www.superherodb.com/pictures2/portraits/10/100/1521.jpg"
+    // },
+    // {
+    //     "response": "success",
+    //     "id": "137",
+    //     "name": "Brainiac 5",
+    //     "url": "https://www.superherodb.com/pictures2/portraits/10/100/1183.jpg"
+    // },
+    // {
+    //     "response": "success",
+    //     "id": "99",
+    //     "name": "Black Cat",
+    //     "url": "https://www.superherodb.com/pictures2/portraits/10/100/32.jpg"
+    // },
+    // {
+    //     "response": "success",
+    //     "id": "89",
+    //     "name": "Bird-Man",
+    //     "url": "https://www.superherodb.com/pictures2/portraits/10/100/1503.jpg"
+    // }
 ])
 
-//     useEffect(()=>{
-//         async function fetch_heros(){
-//            const response = await fetch("http://localhost:4000/home") 
-//            const resData = await response.json();
-//            console.log(resData)
-//            }
-//            fetch_heros()
-//    },[])
+    useEffect(()=>{
+        async function fetch_heros(){
+           const response = await fetch("http://localhost:4000/home") 
+           const resData = await response.json();
+           setHero(resData)
+           setLoading(false)
+           }
+          fetch_heros()
+           
+   },[])
 
 console.log(hero)
 
 
-const galleryItems = hero.map(hero =>{
+const galleryItems = hero.map((hero, i) =>{
     return(
-    <GalleryItem hero = {hero} /> 
+        <GalleryItem hero = {hero} key = {i} /> 
+
     )
 })
 
     return(
+        <div className="center">
         <div className="gallery-container">
             {galleryItems}
+            </div>
         </div>
     )
 }
--- src/components/galleryItem.js
+++ src/components/galleryItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 import '../css/gallery.css'
+import {Link} from "react-router-dom"
 
-function GalleryItem({hero}){
-
+function GalleryItem({hero, key}){
+console.log(hero)
     return(
-        <div className="card-container">
+        <div key={key} className="card-container">
             <h3>{hero.name}</h3>
+            <Link to={`/${hero.id}`}>
                 <img classname = "hero-img" src = {hero.url} alt= {hero.name}/>
+            </Link>  
         </div>
     )
 }
